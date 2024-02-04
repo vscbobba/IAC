e7d2f8569c88bd31040aea63330c450d225298c0
chowdarybvsn <chowdarybvsn@gmail.com>
2024-02-04 20:07:51 +0530

    VPC peering

--- main.tf
+++ main.tf
@@ -54,7 +54,6 @@ resource "aws_route_table_association" "pub_subnet_associate" {
     route_table_id = aws_route_table.Main.id  
 }
 
-
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
@@ -88,7 +87,7 @@ resource "aws_instance" "frontend" {
     ami = var.ami
     instance_type = var.inst
     security_groups = [aws_security_group.allow_tls.id]
-    subnet_id = aws_subnet.Public_subnet.id
+    subnet_id = aws_subnet.Private_subnet1.id
    provisioner "remote-exec" {
      inline = [
             "sudo dnf install nginx -y",
@@ -116,4 +115,24 @@ resource "aws_instance" "frontend" {
       host     = self.public_ip
      }
    }
+}
+
+resource "aws_vpc_peering_connection" "VPC_peer" {
+  vpc_id          = "vpc-013b83a7327f3ca6a"
+  peer_vpc_id     = aws_vpc.IAC.id
+}
+resource "aws_vpc_peering_connection_accepter" "example" {
+  provider          = aws
+  vpc_peering_connection_id = aws_vpc_peering_connection.VPC_peer.id
+}
+
+resource "aws_route" "workstation_to_ec2" {
+  route_table_id         = "rtb-06e7d4ebf3b36ee61"
+  destination_cidr_block = aws_vpc.IAC.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.VPC_peer.id
+}
+resource "aws_route" "ec2_to_workstation" {
+  route_table_id         = aws_route_table.Main.id
+  destination_cidr_block = "30.0.0.0/16"
+  vpc_peering_connection_id = aws_vpc_peering_connection_accepter.example.id
 }
