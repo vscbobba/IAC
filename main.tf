5061ecf85e3aad081130e1d2bd3b4c036523e7f9
chowdarybvsn <chowdarybvsn@gmail.com>
2024-02-04 12:49:17 +0530

    IGW,routetable,pubsub association

--- main.tf
+++ main.tf
@@ -35,3 +35,22 @@ resource "aws_subnet" "Private_subnet3" {
     Name = "MyPriv3"
    }
 }
+
+resource "aws_internet_gateway" "MY_IGW"{
+    vpc_id = aws_vpc.IAC.id
+    tags ={
+        Name = "MY-IGW"
+    }
+}
+resource "aws_route_table" "Main" {
+   vpc_id = aws_vpc.IAC.id
+   route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.MY_IGW.id
+   }  
+}
+resource "aws_route_table_association" "pub_subnet_associate" {
+    subnet_id = aws_subnet.Public_subnet.id
+    route_table_id = aws_route_table.Main.id  
+}
+
