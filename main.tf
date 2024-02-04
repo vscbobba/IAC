eded0271bf43192be3a25f98d1e2e3fdb279bf0c
chowdarybvsn <chowdarybvsn@gmail.com>
2024-02-04 12:39:59 +0530

    subnets setup done

--- main.tf
+++ main.tf
@@ -4,3 +4,34 @@ resource "aws_vpc" "IAC" {
         name = "MyVPC"
     }   
 }
+
+resource "aws_subnet" "Public_subnet" {
+   cidr_block = var.public_cidr
+   vpc_id = aws_vpc.IAC.id
+   map_public_ip_on_launch = true
+   tags ={
+    Name = "MyPub"
+   }
+}
+
+resource "aws_subnet" "Private_subnet1" {
+   cidr_block = var.Priv_sn1
+   vpc_id = aws_vpc.IAC.id
+   tags ={
+    Name = "MyPriv1"
+   }
+}
+resource "aws_subnet" "Private_subnet2" {
+   cidr_block = var.Priv_sn2
+   vpc_id = aws_vpc.IAC.id
+   tags ={
+    Name = "MyPriv2"
+   }
+}
+resource "aws_subnet" "Private_subnet3" {
+   cidr_block = var.Priv_sn3
+   vpc_id = aws_vpc.IAC.id
+   tags ={
+    Name = "MyPriv3"
+   }
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,20 @@ variable "vpc_cidr" {
     default = "20.0.0.0/16"
     type = string
 }
+variable "public_cidr" {
+    default = "20.0.10.0/24"
+    type = string
+}
+
+variable "Priv_sn1" {
+    default = "20.0.1.0/24"
+    type = string
+}
+variable "Priv_sn2" {
+    default = "20.0.2.0/24"
+    type = string
+}
+variable "Priv_sn3" {
+    default = "20.0.3.0/24"
+    type = string
+}
