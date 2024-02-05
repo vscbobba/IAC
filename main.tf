d16827939fa381a512c73fb003b2a4b43943eb90
chowdarybvsn <chowdarybvsn@gmail.com>
2024-02-05 23:57:20 +0530

    $ issue amazing

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_instance" "DB" {
               sudo tee -a /root/mysql.repo <<EOL
               [mysql]
               name=MySQL 5.7 Community Server
-              baseurl="http://repo.mysql.com/yum/mysql-5.7-community/el/7/$basearch/"
+              baseurl=http://repo.mysql.com/yum/mysql-5.7-community/el/7/\$basearch/
               enabled=1
               gpgcheck=0
               EOL
