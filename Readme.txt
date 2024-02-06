e63fe652917d854eea72cf491710f560df75c267
chowdarybvsn <chowdarybvsn@gmail.com>
2024-02-06 23:19:19 +0530

    setup tags and inventory

--- Readme.txt
+++ Readme.txt
@@ -1,7 +1,3 @@
-frontend ansible_host=20.0.10.231 ansible_user=centos ansible_ssh_pass=DevOps321
-DB ansible_host=20.0.1.88 ansible_user=centos ansible_ssh_pass=DevOps321
-backend ansible_host=20.0.2.59 ansible_user=centos ansible_ssh_pass=DevOps321
-
 playbook.yml
 
 - name: start playbook
@@ -16,6 +12,6 @@ playbook.yml
   hosts: "{{ anshost }}"
   roles:
      - "{{ role_name }}"
-     
+
 
 ansible-playbook playbook.yml -e anshost=backend -e "role_name=testing"
--- main.tf
+++ main.tf
@@ -10,6 +10,9 @@ resource "aws_instance" "bastion" {
     security_groups = [module.Network.SG]
     subnet_id = module.Network.Public_subnet1
     iam_instance_profile = data.aws_iam_role.example-role.name
+    tags = {
+      Name = "bastion"
+    }
     user_data = <<-EOF
               #!/bin/bash
               sudo set-hostname workstation
@@ -17,6 +20,7 @@ resource "aws_instance" "bastion" {
               sudo yum install -y yum-utils
               sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
               sudo yum -y install terraform
+              sudo cat /root/IAC/Ansible/inventory-info >>/etc/ansible/hosts
               EOF
 }
 resource "aws_instance" "frontend" {
@@ -25,6 +29,9 @@ resource "aws_instance" "frontend" {
     security_groups = [module.Network.SG]
     iam_instance_profile = data.aws_iam_role.example-role.name
     subnet_id = module.Network.Public_subnet1
+    tags ={
+        Name = "frontend"
+    }
     user_data = <<-EOF
               #!/bin/bash
               sudo set-hostname frontend
@@ -36,6 +43,9 @@ resource "aws_instance" "DB" {
     security_groups = [module.Network.SG]
     subnet_id = module.Network.priv_subnet1
     iam_instance_profile = data.aws_iam_role.example-role.name
+    tags = {
+      Name = "DB"
+    }
     user_data = <<-EOF
               #!/bin/bash
               sudo set-hostname DB
@@ -47,6 +57,9 @@ resource "aws_instance" "backend" {
     security_groups = [module.Network.SG]
     subnet_id = module.Network.priv_subnet2
     iam_instance_profile = data.aws_iam_role.example-role.name
+    tags = {
+      Name = "backend"
+    }
     user_data = <<-EOF
              #!/bin/bash
              sudo set-hostname backend
