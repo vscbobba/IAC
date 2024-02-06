5591c4f03f1dea6a46f216dd4bc79d94c97b96a0
chowdarybvsn <chowdarybvsn@gmail.com>
2024-02-06 23:20:57 +0530

    revert

--- main.tf
+++ main.tf
@@ -20,7 +20,6 @@ resource "aws_instance" "bastion" {
               sudo yum install -y yum-utils
               sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
               sudo yum -y install terraform
-              sudo cat /root/IAC/Ansible/inventory-info >>/etc/ansible/hosts
               EOF
 }
 resource "aws_instance" "frontend" {
